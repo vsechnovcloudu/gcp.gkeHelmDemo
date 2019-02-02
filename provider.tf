4a5b57d80901e3cc9de0ea5334c323497591d0c4
David Kubec <davesade.42@gmail.com>
2019-02-02 14:22:13 +0100

    Updated provider.

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
- 
-  project     = "prgandrs-1545764074679"
-  region      = "europe-west2-a"
+  # credentials = "${file("prgandrs-1545764074679-0477023800bb.json")}"
+  project     = "bee-hypo-${terraform.workspace}"
+  region      = "europe-west3"
 }
